d9f5e0980b3add90d881619f70c8ee9168ef89e1
Ronan Fernandes Machado <ronanf.sk@gmail.com>
2020-03-27 13:28:05 -0300

    Resolvido exercicio 4 e modificado concatenação dos exercicios 2 e 3

--- exercises/4_1/ex02.js
+++ exercises/4_1/ex02.js
@@ -2,9 +2,9 @@ let a = 3;
 let b = 10;
 
 if (a > b) {
-    console.log(a, "é maior que ", b);
+    console.log(a + " é maior que ", b);
 }else if (b > a){
-    console.log(b, "é maior que ", a);
+    console.log(b + " é maior que ", a);
 } else {
-    console.log(a, "é igual a ", b);
+    console.log(a + " é igual a ", b);
 }
