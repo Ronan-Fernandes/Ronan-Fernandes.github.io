0ec58e645e1acf43333956a48ae2d148fdd4baaa
Ronan Fernandes Machado <ronanf.sk@gmail.com>
2020-03-27 12:30:26 -0300

    Resolvido exercicio 2

---
+++ exercises/4_1/ex02.js
@@ -0,0 +1,10 @@
+let a = 3;
+let b = 10;
+
+if (a > b) {
+    console.log(a, "é maior que ", b);
+}else if (b > a){
+    console.log(b, "é maior que ", a);
+} else {
+    console.log(a, "é igual a ", b);
+}
