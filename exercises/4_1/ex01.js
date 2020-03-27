d44a1618d96baf42e8c2762b3879e928969bd046
Ronan Fernandes Machado <ronanf.sk@gmail.com>
2020-03-27 12:20:45 -0300

    Resolução do exerciio 1

---
+++ exercises/4_1/ex01.js
@@ -0,0 +1,27 @@
+// Adição 
+
+let a = 10;
+let b = 5;
+
+let soma = a + b;
+console.log("Soma = ", soma);
+
+// subtração 
+
+let subtracao = a - b;
+console.log("subtração = ", subtracao);
+
+// multiplicação
+
+let multiplicacao = a * b;
+console.log("multiplicção = ", multiplicacao);
+
+// Dvisão 
+
+let divisao = a / b;
+console.log("Divisão = ", divisao);
+
+// Módulo 
+
+let modulo = a % b;
+console.log("Módulo = ", modulo);
