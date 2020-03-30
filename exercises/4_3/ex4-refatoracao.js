7c9512e17120bdfc12b32a7de96fed66b3d39e11
Ronan Fernandes Machado <ronanf.sk@gmail.com>
2020-03-30 18:09:31 -0300

    refatorado exercicio 4

---
+++ exercises/4_3/ex4-refatoracao.js
@@ -0,0 +1,11 @@
+let n = 0;
+function verificaSinal(x) {
+    if (x > 0) {
+        console.log("positive");
+    } else if (x < 0) {
+        console.log("negative");
+    } else {
+        console.log("zero");
+    }
+} 
+verificaSinal(n);
