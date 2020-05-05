36bb92967558e4fd563336646f730d2dce15df7a
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-05-05 20:53:41 -0300

    exercicio 6

--- exercises/8_3/ex-6.js
+++ exercises/8_3/ex-6.js
@@ -10,3 +10,9 @@ function myFizzBuzz(num) {
 
 // implemente seus testes aqui
 assert.strictEqual(myFizzBuzz(15), 'fizzbuzz');
+assert.strictEqual(myFizzBuzz(9), 'fizz');
+assert.strictEqual(myFizzBuzz(25), 'buzz');
+assert.strictEqual(myFizzBuzz(7), 7);
+assert.strictEqual(myFizzBuzz('9'), false);
+
+
