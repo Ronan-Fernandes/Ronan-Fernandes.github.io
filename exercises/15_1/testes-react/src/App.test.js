10bc5140495ae3b4855b32b3cbf120fc14b6c679
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-06-25 10:58:42 -0300

    testando comportamento

--- exercises/15_1/testes-react/src/App.test.js
+++ exercises/15_1/testes-react/src/App.test.js
@@ -1,6 +1,6 @@
 // App.test.js
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('Verificando se existe o campo Email.', () => {
@@ -22,4 +22,20 @@ test('Verificando se existe dois botões', () => {
   const { getAllByRole } = render(<App />);
   const buttons = getAllByRole('button');
   expect(buttons.length).toBe(2);
+})
+
+test('Verificando se o botão e o email estão funcionando.', () => {
+  const { getByTestId, getByLabelText } = render(<App />);
+  const EMAIL_USER = 'email@email.com';
+
+  const textEmail = getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail.textContent).toBe('Valor: ');
+
+  const btnSend = getByTestId('id-send');
+  const inputEmail = getByLabelText('Email');
+  fireEvent.change(inputEmail, { target: {value: EMAIL_USER } });
+  fireEvent.click(btnSend);
+  expect(inputEmail.value).toBe('');
+  expect(textEmail.textContent).toBe(`Valor: ${EMAIL_USER}`);
 })
