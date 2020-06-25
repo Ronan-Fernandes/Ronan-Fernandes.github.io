710a29865df1f00fb1886cf4a1aa802c0addce3d
Ronan-Fernandes <ronanf.sk@gmail.com>
2020-06-25 10:39:07 -0300

    first part of the course exemples

---
+++ exercises/15_1/testes-react/src/App.js
@@ -0,0 +1,18 @@
+    // App.js
+    import React from 'react';
+    import './App.css';
+    
+    function App() {
+      return (
+        <div className="App">
+          <label htmlFor="id-email">
+            Email
+          </label>
+          <input id="id-email" type="email" />
+          <input id="btn-send" data-testid="id-send" type="button" value="Enviar" />
+          <input id="btn-back" type="button" value="Voltar" />
+        </div>
+      );
+    }
+    
+    export default App;
